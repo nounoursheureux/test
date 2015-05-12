c96e27fa78001a323732c8116c6c0f1046ddafc7
Nounours Heureux <schtroumps31@gmail.com>
2015-05-12 19:35:58 +0200

    svdsvd

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $('#btn').click(function(e){
     console.log('sending');
-    $.get('https://api.github.com',function(data){
+    $.get('https://github.com',function(data){
         console.log(data);
     });
 });
